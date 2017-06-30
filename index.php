1aa6e3e85ecd62fa32bb8bee94bfc41b6c023110
Aryslied <aryslied@gmail.com>
2017-06-30 15:30:50 +0200

    enfin

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+error_reporting(E_ALL);
+?>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Black Standard Clock</title>
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
+</head>
+
+<body>
+
+<div class="container">
+
+    <div class="navbar navbar-fixed-top">
+        <div class="container" style="margin-top:2%;">
+     <span style="float:right; margin-right: -166px;">
+
+
+
+<div style="margin-left: 884px;">
+<?php require 'city.php'; ?>
+</div>
+<span style="margin-left:809px;"><?php require_once 'login_status.php'; ?></span>
+   </span>
+
+            <div class="Fo_nt " style=" color:#faa300; font-family:'Comic Sans MS', cursive; font-weight:bold; margin-left:10%; width:100px;"><span class="span8">My</span></div>
+            <div class="Fo_nt" style="color:#79ab00; margin-left:20px; font-family:'Comic Sans MS', cursive; font-style:italic;"><span class="span1">Health</span></div>
+            <div class="Fo_nt" style="color:#1da2d8;  width:100px; font-family:'Comic Sans MS', cursive; font-style:italic;"><span class="span2" style="margin-left:163px;">Package</span></div>
+
+        </div>
+    </div>
+</div>
+
+
+
+
+<div style="clear:both; "></div>
+
+
+<div class="container">
+    <div class="hero-unit1 h_top2 r_b2" style="background:none!important; border-radius:8px; height:40px; color:#fff; font-weight:bold;" >
+        <div class="row">
+
+
+        </div>
+    </div>
