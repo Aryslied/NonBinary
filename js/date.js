f3484c7777c5458fc3da21986b6da0b00c514030
Aryslied <aryslied@gmail.com>
2017-07-01 01:29:57 +0200

    un petit message

--- js/date.js
+++ js/date.js
@@ -2,11 +2,25 @@
 Give me the time mothafucka
  */
 
-
+$(function() {
+    setInterval(function(){ getTime(); }, 1000);
 
 function getTime() {
-    var time = new Date(hours,minutes);
+    var time = new Date();
     var hours = time.getHours();
     var minutes = time.getMinutes();
-    return hours+':'+minutes
+    var seconds = time.getSeconds();
+    var date = hours+':'+minutes+':'+seconds;
+
+    $('#time').html('<h1>'+date+'</h1>');
+
+
+
+    console.log(date);
 }
+//
+// function delay() {
+//     timeoutID = window.setTimeout(getTime, 1000);
+// }
+
+});
