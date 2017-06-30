28bb71900fa8a34b9eb94c6e26337d6252568db1
Aryslied <aryslied@gmail.com>
2017-06-30 21:05:14 +0200

    div

--- index.php
+++ index.php
@@ -9,8 +9,8 @@ error_reporting(E_ALL);
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Black Standard Clock</title>
-    <link href="..css/bootstrap.css" rel="stylesheet">
-    <link href="..css/styles.css" rel="stylesheet">
+    <link href="./css/bootstrap.css" rel="stylesheet">
+    <link href="./css/styles.css" rel="stylesheet">
 </head>
 
 <body>
--- js/date.js
+++ js/date.js
@@ -2,6 +2,8 @@
 Give me the time mothafucka
  */
 
+
+
 function getTime() {
     var time = new Date(hours,minutes);
     var hours = time.getHours();
