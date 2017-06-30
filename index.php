508833f9333a9a579a842c1e6f36e44ba51b36cd
Aryslied <aryslied@gmail.com>
2017-06-30 23:42:59 +0200

    nothing

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ error_reporting(E_ALL);
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Black Standard Clock</title>
     <link href="./css/bootstrap.css" rel="stylesheet">
-    <link href="./css/styles.css" rel="stylesheet">
+    <link href="./css/style.css" rel="stylesheet">
 </head>
 
 <body>
