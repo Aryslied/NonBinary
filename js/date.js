1cb9a51f95e091f42d1bdcc1046666218b89b979
Aryslied <aryslied@gmail.com>
2017-07-03 18:01:11 +0200

    jauge des sec

--- js/date.js
+++ js/date.js
@@ -12,10 +12,15 @@ $(function() {
 
         $('#time').html('<h1>' + date + '</h1>');
 
-        getBinaryTime(date);
+        var seconds = getBinaryTime(date);
 
-        //Show in in console
-        //console.log(date);
+        //Put seconds in ya donut Jeez
+        $(function() {
+            sec.refresh(seconds);
+        });
+
+        // Show in in console
+        // console.log(date);
     }
 
 });
@@ -36,12 +41,32 @@ function getBinaryTime(d) {
     var minBin = dec2bin (minutes);
     var secBin = dec2bin (seconds);
 
-    //Displays it
-    $('#hBin').html('<h2>' + hourBin + '</h2>');
-    $('#mBin').html('<h2>' + minBin + '</h2>');
-    $('#sBin').html('<h2>' + secBin + '</h2>');
-
     //In 1 line, with format, displayed
     var timeBin = hourBin.toString() + ':' + minBin.toString() + ':' + secBin.toString();
     $('#timeBin').html('<h2>' + timeBin + '</h2>');
+
+    return seconds;
 }
+
+//Just create ya shitty doughnut
+var sec;
+$(function() {
+    sec = new JustGage ({
+        value: 0,
+        id: "secGauge",
+        donut: true,
+        min: 0,
+        max: 59,
+        gaugeWidthScale: 0.4,
+        shadowOpacity: 1,
+        shadowSize: 6,
+        startAnimationTime: 1000,
+        refreshAnimationTime: 1000,
+        levelColors: [
+            "#666666",
+            "#006699"
+        ],
+        hideInnerShadow: true
+    });
+
+});
