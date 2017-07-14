1e3d3e0c4cf12159d958fc4b4984e4b94c08f406
Aryslied <aryslied@gmail.com>
2017-07-14 15:02:11 +0200

    Whole new stuff in js

--- js/canvas.js
+++ js/canvas.js
@@ -4,86 +4,98 @@ $(function() {
     var c = $("#myCanvas");
     var context = c.get(0).getContext("2d");
 
+    var xyHalf = 300; // half of x and y max
+    var lineWidth = 80;
+    var pi = Math.PI;
+
 // Clock hours
     //1
     context.beginPath();
-    context.arc(300, 300, 300, Math.PI, Math.PI+Math.PI/2,true);
-    context.moveTo(300, 0); // Set the path origin
-    context.lineTo(300, 300); // Set the path destination
-    context.lineTo(600, 300);
+    context.arc(xyHalf, xyHalf, xyHalf, pi, pi*3/2,true);
+    context.moveTo(xyHalf, 0);
+    context.lineTo(xyHalf, xyHalf);
+    context.lineTo(xyHalf*2, xyHalf);
     context.fillStyle = "cyan";
     context.closePath();
     context.fill();
 
     //2
     context.beginPath();
-    context.arc(300, 300, 300, 0, Math.PI/2);
-    context.moveTo(600, 300); // Set the path origin
-    context.lineTo(300, 300); // Set the path destination
-    context.lineTo(300, 600);
+    context.arc(xyHalf, xyHalf, xyHalf, 0, pi/2);
+    context.moveTo(xyHalf*2, xyHalf);
+    context.lineTo(xyHalf, xyHalf);
+    context.lineTo(xyHalf, xyHalf*2);
     context.closePath();
     context.fill();
 
     //4
     context.beginPath();
-    context.arc(300, 300, 300, Math.PI, Math.PI/2,true);
-    context.moveTo(0, 300); // Set the path origin
-    context.lineTo(300, 300); // Set the path destination
-    context.lineTo(300, 600);
+    context.arc(xyHalf, xyHalf, xyHalf, pi, pi/2,true);
+    context.moveTo(0, xyHalf);
+    context.lineTo(xyHalf, xyHalf);
+    context.lineTo(xyHalf, xyHalf*2);
     context.closePath();
     context.fill();
 
     //8
     context.beginPath();
-    context.arc(300, 300, 300, Math.PI, Math.PI+Math.PI/2);
-    context.moveTo(0, 300); // Set the path origin
-    context.lineTo(300, 300); // Set the path destination
-    context.lineTo(300, 0);
+    context.arc(xyHalf, xyHalf, xyHalf, pi, pi+pi/2);
+    context.moveTo(0, xyHalf);
+    context.lineTo(xyHalf, xyHalf);
+    context.lineTo(xyHalf, 0);
     context.closePath();
     context.fill();
 
 // Clock big black cross using lines to center it easily
-    context.lineWidth = 80;
+    context.lineWidth = lineWidth;
 
     //Vertical
     context.beginPath();
-    context.moveTo(300, 0);
-    context.lineTo(300, 600);
+    context.moveTo(xyHalf, 0);
+    context.lineTo(xyHalf, xyHalf*2);
     context.closePath();
     context.stroke();
 
     //Horizontal
     context.beginPath();
-    context.moveTo(0, 300);
-    context.lineTo(600, 300);
+    context.moveTo(0, xyHalf);
+    context.lineTo(xyHalf*2, xyHalf);
     context.closePath();
     context.stroke();
 
 // Clock minutes
-    context.lineWidth = 40;
+    context.lineWidth = lineWidth/2;
 
     //Vertical
 
     //Horizontal
+    //30
     context.strokeStyle = "red";
     context.beginPath();
-    //context.arc(300, 300, 300, Math.PI-Math.PI/4, Math.PI+Math.PI/4)
-    context.moveTo(30, 300);
-    context.lineTo(250, 300);
+    //context.arc(xyHalf, xyHalf, xyHalf, pi-, pi+)
+    context.moveTo(30, xyHalf);
+    context.lineTo(300, xyHalf);
     context.closePath();
     context.stroke();
 
+    //15
     context.beginPath();
-    //context.arc(300, 300, 300, Math.PI-Math.PI/4, Math.PI+Math.PI/4)
-    context.moveTo(350, 300);
-    context.lineTo(570, 300);
+    //context.arc(xyHalf, xyHalf, xyHalf, pi-pi/4, pi+pi/4)
+    context.moveTo(300, xyHalf);
+    context.lineTo(570, xyHalf);
     context.closePath();
     context.stroke();
 
+// Clock black circle center
+    context.beginPath();
+    context.arc(xyHalf, xyHalf, lineWidth/2, 0, pi*2);
+    context.fillStyle = "black";
+    context.closePath();
+    //context.fill();
 
 // Clock button
     context.beginPath();
-    context.arc(300, 300, 15, 0, Math.PI*2);
+    context.arc(xyHalf, xyHalf, 15, 0, pi*2);
     context.fillStyle = "purple";
     context.closePath();
     context.fill();
