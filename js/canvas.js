a9ebb268329e994a750fa6ab1a3dd20e13ced964
Aryslied <aryslied@gmail.com>
2017-07-14 03:57:02 +0200

    Beautiful hours

--- js/canvas.js
+++ js/canvas.js
@@ -5,32 +5,64 @@ $(function() {
     var context = c.get(0).getContext("2d");
     // context.lineWidth = 5;
 
-// Background
-    //context.fillRect(50,50,250,250);
-
-
-// Clock background
-    context.arc(300, 300, 300, 0, Math.PI*2);//note: too small with 300
-    context.fill();
-
 // Clock hours
+    //1
     context.beginPath();
-    context.arc(300, 300, 300, Math.PI, Math.PI+Math.PI/2);
-    context.moveTo(0, 300); // Set the path origin
+    context.arc(300, 300, 300, Math.PI, Math.PI+Math.PI/2,true);
+    context.moveTo(300, 0); // Set the path origin
     context.lineTo(300, 300); // Set the path destination
-    context.fillStyle = "cyan";
+    context.lineTo(600, 300);
+    context.fillStyle = "blue";
     context.closePath();
     context.fill();
 
-
-//Clock big black cross
+    //2
     context.beginPath();
-    context.lineWidth = 10;
-    context.moveTo(300, 0);
+    context.arc(300, 300, 300, 0, Math.PI/2);
+    context.moveTo(600, 300); // Set the path origin
+    context.lineTo(300, 300); // Set the path destination
+    context.lineTo(300, 600);
+    context.fillStyle = "yellow";
+    context.closePath();
+    context.fill();
+
+    //4
+    context.beginPath();
+    context.arc(300, 300, 300, Math.PI, Math.PI/2,true);
+    context.moveTo(0, 300); // Set the path origin
+    context.lineTo(300, 300); // Set the path destination
     context.lineTo(300, 600);
     context.fillStyle = "red";
     context.closePath();
     context.fill();
+
+    //8
+    context.beginPath();
+    context.arc(300, 300, 300, Math.PI, Math.PI+Math.PI/2);
+    context.moveTo(0, 300); // Set the path origin
+    context.lineTo(300, 300); // Set the path destination
+    context.lineTo(300, 0);
+    context.fillStyle = "cyan";
+    context.closePath();
+    context.fill();
+
+//Clock big black cross
+    //context.strokeStyle = "red"; //test line: to see it red
+    context.lineWidth = 80;
+
+    //Vertical
+    context.beginPath();
+    context.moveTo(300, 0);
+    context.lineTo(300, 600);
+    context.closePath();
+    context.stroke();
+
+    //Horizontal
+    context.beginPath();
+    context.moveTo(0, 300);
+    context.lineTo(600, 300);
+    context.closePath();
+    context.stroke();
 
 // Clock button
     context.beginPath();
@@ -42,6 +74,6 @@ $(function() {
 
 
     $( "roundButton" ).click(function() {
-        //
+        //on click display the clock
     })
 });
